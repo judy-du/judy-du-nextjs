56afbb06d4c8070b2a08f8def8080c0b4d645d72
Victor Reipur <victor.reipur@gmail.com>
2024-10-13 11:02:49 +0000

    initial commit

---
+++ app-main/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold text-center text-blue-600">
+          Hello World, I support Tailwind!
+        </h1>
+      </div>
+    );
+  }
+  
